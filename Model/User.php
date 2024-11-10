4969f40f92492f967129402799bfea8094057660
Haidoan29 <129633565+Haidoan29@users.noreply.github.com>
2024-11-10 15:00:56 +0700

    fix sha1

--- Model/User.php
+++ Model/User.php
@@ -9,9 +9,13 @@ class User {
     }
 
     public function checkLogin($ten_dang_nhap, $mat_khau) {
+        // Mã hóa mật khẩu người dùng nhập vào bằng SHA-1
+        $mat_khau_ma_hoa = sha1($mat_khau);
+
+        // Thực hiện truy vấn
         $query = "SELECT * FROM nguoidung WHERE ten_dang_nhap = ? AND mat_khau = ?";
         $stmt = $this->conn->prepare($query);
-        $stmt->bind_param('ss', $ten_dang_nhap, $mat_khau);
+        $stmt->bind_param('ss', $ten_dang_nhap, $mat_khau_ma_hoa);
         $stmt->execute();
         return $stmt->get_result();
     }
